21f7d94655b1a1bb0f6ff30b15c4234598e1e20d
yechanpark <qkrdpcks0419@naver.com>
2019-07-09 00:17:08 +0900

    풀이법 추가

--- src/main/java/codility/lessons/lesson5_prefixsums/MinAvgTwoSlice.java
+++ src/main/java/codility/lessons/lesson5_prefixsums/MinAvgTwoSlice.java
@@ -42,6 +42,14 @@ package codility.lessons.lesson5_prefixsums;
  * each element of array A is an integer within the range [−10,000..10,000].
  */
 
+
+/**
+ * a <= b일 때, a와 b의 평균은 a 이상이 된다. (a = b 일 때, a와 b의 평균은 a, 즉 두 수가 같은 경우는 a 혹은 b가 된다)
+ * 마찬가지로, (a + b) <= (c + d)일 때, (a, b)와 (c, d)의 평균은 (a + b) 이상이 된다.
+ * 결국, 원소가 4개(a, b, c, d)인 그룹은 (a, b)와 (c, d)를 나누었을 때, 각각의 평균의 작은 값 이상이 된다.
+ * 2개인 그룹이 작은 값이 되므로 4개의 그룹은 고려할 필요가 없어진다.
+ * 예외로 원소가 3개인 그룹에서 2개인 그룹과 1개인 그룹의 경우를 확인해야 하지만, 문제에서 주어진 조건에 의하면 그룹의 원소는 최소 2개 이상이므로 2개와 3개인 그룹만 비교한다.
+ * */
 public class MinAvgTwoSlice {
     public int solution(int[] A) {
         return 1;
