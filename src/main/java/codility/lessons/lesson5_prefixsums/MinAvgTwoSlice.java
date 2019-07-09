1406c51656d856f4e8ac0d9b603780e4e84fa33a
yechanpark <qkrdpcks0419@naver.com>
2019-07-09 10:46:10 +0900

    Codility - MinAvgTwoSlice

--- src/main/java/codility/lessons/lesson5_prefixsums/MinAvgTwoSlice.java
+++ src/main/java/codility/lessons/lesson5_prefixsums/MinAvgTwoSlice.java
@@ -44,14 +44,39 @@ package codility.lessons.lesson5_prefixsums;
 
 
 /**
+ * 풀이법
  * a <= b일 때, a와 b의 평균은 a 이상이 된다. (a = b 일 때, a와 b의 평균은 a, 즉 두 수가 같은 경우는 a 혹은 b가 된다)
  * 마찬가지로, (a + b) <= (c + d)일 때, (a, b)와 (c, d)의 평균은 (a + b) 이상이 된다.
  * 결국, 원소가 4개(a, b, c, d)인 그룹은 (a, b)와 (c, d)를 나누었을 때, 각각의 평균의 작은 값 이상이 된다.
  * 2개인 그룹이 작은 값이 되므로 4개의 그룹은 고려할 필요가 없어진다.
  * 예외로 원소가 3개인 그룹에서 2개인 그룹과 1개인 그룹의 경우를 확인해야 하지만, 문제에서 주어진 조건에 의하면 그룹의 원소는 최소 2개 이상이므로 2개와 3개인 그룹만 비교한다.
- * */
+ */
 public class MinAvgTwoSlice {
     public int solution(int[] A) {
-        return 1;
+        int minIndex = Integer.MAX_VALUE;
+        double minValue = Float.MAX_VALUE;
+
+        // A.length = 7
+        // i = 0 ~ 6
+        for (int i = 0; i < A.length - 1; i++) {
+            // i = 0 ~ 5일때
+            if (i <= A.length - 2) {
+                double digit2Avg = (A[i] + A[i + 1]) / 2.0;
+                if (digit2Avg < minValue) {
+                    minValue = digit2Avg;
+                    minIndex = i;
+                }
+            }
+
+            // i = 0 ~ 4일때
+            if (i <= A.length - 3) {
+                double digit3Avg = (A[i] + A[i + 1] + A[i + 2]) / 3.0;
+                if (digit3Avg < minValue) {
+                    minValue = digit3Avg;
+                    minIndex = i;
+                }
+            }
+        }
+        return minIndex;
     }
 }
