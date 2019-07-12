564e9f7a09dcedcdbbee1043bbd8ff9852886e63
yechanpark <qkrdpcks0419@naver.com>
2019-07-13 03:08:15 +0900

    Flags 구현중

---
+++ src/main/java/test/lessons/lesson10_prime_and_composite_numbers/FlagsTest.java
@@ -0,0 +1,25 @@
+package test.lessons.lesson10_prime_and_composite_numbers;
+
+import codility.lessons.lesson10_prime_and_composite_numbers.Flags;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class FlagsTest {
+    private Flags flags;
+
+    @BeforeClass
+    public void init() {
+        this.flags = new Flags();
+    }
+
+    @Test
+    public void sample1Test() {
+        int[] A = {1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+        int got = flags.solution(A);
+        int answer = 3;
+        assertEquals(got, answer);
+    }
+
+}
