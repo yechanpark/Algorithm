252e6c5671c8a1c27b6c34b94fdbfb2b6cb0c3e3
yechanpark <qkrdpcks0419@naver.com>
2019-07-13 18:47:04 +0900

    fixes #24

--- src/main/java/codility/lessons/lesson6_sorting/NumberOfDiscIntersections.java
+++ src/main/java/codility/lessons/lesson6_sorting/NumberOfDiscIntersections.java
@@ -1,6 +1,6 @@
 package codility.lessons.lesson6_sorting;
 
-import java.util.*;
+import java.util.Arrays;
 
 /**
  * We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
@@ -34,66 +34,38 @@ import java.util.*;
  * <p>
  * N is an integer within the range [0..100,000];
  * each element of array A is an integer within the range [0..2,147,483,647].
+ *
+ * @see <a href="https://jobjava00.github.io/algorithm/codility/lesson6/NumberOfDiscIntersections/">참고</a>
  */
 
 public class NumberOfDiscIntersections {
     // X축에서 원이 겹치는 쌍의 갯수를 구함
     public int solution(int[] A) {
+        int N = A.length;
+        long[] lower = new long[N];
+        long[] upper = new long[N];
 
-        // Key -> X축 값, Value -> 해당 X축에 포함되는 원의 인덱스 넘버 리스트
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        // 현재 J 기준으로 반지름을 구해서 lower, upper 배열에 담는다
+        for (int i = 0; i < N; i++) {
+            lower[i] = i - (long) A[i];
+            upper[i] = i + (long) A[i];
+        }
 
-        // 모든 선을 순회하면서
-        for (int i = 0; i < A.length; i++) {
+        Arrays.sort(lower);
+        Arrays.sort(upper);
 
-            for (int j = 0; j <= A[i]; j++) {
-                // X축 양의 값에 현재 원의 인덱스 번호를 넣음
-                if (map.get(i + j) == null)
-                    map.put(i + j, new ArrayList<>());
-                map.get(i + j).add(i);
+        int intersection = 0;
+        int j = 0;
 
-                // X축 음의 값에 현재 원의 인덱스 번호를 넣음
-                if (j != 0) {
-                    if (map.get(i - j) == null)
-                        map.put(i - j, new ArrayList<>());
-                    map.get(i - j).add(i);
-                }
+        for (int i = 0; i < N; i++) {
+            while (j < N && upper[i] >= lower[j]) {
+                intersection += j;
+                intersection -= i;
+                j++;
             }
         }
 
-        // [원 인덱스][페어 원 인덱스] -> 여기에 값이 1이면 이미 계산한거임
-        int[][] check = new int[A.length][map.size()];
-
-        int count = 0;
-
-        // x축을 돌면서
-        for (Integer x : map.keySet()) {
-            List<Integer> list = map.get(x);
-            // 2개 이상 원이 겹치지 않으면 카운트하지 않음
-            if (list.size() < 2) continue;
-
-            for (int i = 0; i < list.size() - 1; i++) {
-                for (int j = i + 1; j < list.size(); j++) {
-
-                    // 앞에꺼가 작은 경우
-                    if (list.get(i) < list.get(j)) {
-                        if (check[list.get(i)][list.get(j)] == 0) {
-                            check[list.get(i)][list.get(j)] = 1;
-                            count++;
-                        }
-                    } else {
-                        if (check[list.get(j)][list.get(i)] == 0) {
-                            check[list.get(j)][list.get(i)] = 1;
-                            count++;
-                        }
-                    }
-
-                    // The function should return −1 if the number of intersecting pairs exceeds 10,000,000.
-                    if (count > 10000000) return -1;
-                }
-            }
-        }
-
-        return count;
+        if (intersection > 10000000) return -1;
+        return intersection;
     }
 }
