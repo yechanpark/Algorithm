3a4692075f28e6cc93787c97c4c2b0976199e157
yechanpark <qkrdpcks0419@naver.com>
2020-01-26 02:24:57 +0900

    LeetCode_Easy_PlusOne

---
+++ src/test/java/leetcode/easy/plusone/PlusOneTest.java
@@ -0,0 +1,56 @@
+package leetcode.easy.plusone;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class PlusOneTest {
+    private PlusOne plusOne;
+
+    @BeforeEach
+    public void init() {
+        this.plusOne = new PlusOne();
+    }
+
+    @Test
+    public void sample1Test() {
+        int[] x = { 1, 2, 3 };
+        int[] actual = plusOne.solution(x);
+        int[] expected = { 1, 2, 4 };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sample2Test() {
+        int[] x = { 4, 3, 2, 1 };
+        int[] actual = plusOne.solution(x);
+        int[] expected = { 4, 3, 2, 2 };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sample3Test() {
+        int[] x = { 9 };
+        int[] actual = plusOne.solution(x);
+        int[] expected = { 1, 0 };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sample4Test() {
+        int[] x = { 9, 9 };
+        int[] actual = plusOne.solution(x);
+        int[] expected = { 1, 0, 0 };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void sample5Test() {
+        int[] x = { 8, 9, 9, 9 };
+        int[] actual = plusOne.solution(x);
+        int[] expected = { 9, 0, 0, 0 };
+        assertArrayEquals(expected, actual);
+    }
+
+}
