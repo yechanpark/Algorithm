b0bebed73f6b5beda109585132d21db470570fae
yechanpark <qkrdpcks0419@naver.com>
2020-02-01 19:35:41 +0900

    LeetCode_Easy_ValidPalindrome

---
+++ src/main/java/LeetCode/Easy/String/ValidPalindrome/ValidPalindrome_BestPractice.java
@@ -0,0 +1,17 @@
+package LeetCode.Easy.String.ValidPalindrome;
+
+/**
+ *
+ */
+public class ValidPalindrome_BestPractice {
+    public boolean solution(String s) {
+        char[] c = s.toCharArray();
+        for (int i = 0, j = c.length - 1; i < j; ) {
+            if (!Character.isLetterOrDigit(c[i])) i++;
+            else if (!Character.isLetterOrDigit(c[j])) j--;
+            else if (Character.toLowerCase(c[i++]) != Character.toLowerCase(c[j--]))
+                return false;
+        }
+        return true;
+    }
+}
