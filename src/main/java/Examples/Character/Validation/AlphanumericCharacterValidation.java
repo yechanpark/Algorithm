9d87a58c5644d2da1e72c75a986baff9c75b2dc1
yechanpark <qkrdpcks0419@naver.com>
2020-02-01 19:37:16 +0900

    Character.isLetterOrDigit() 사용법 추가

---
+++ src/main/java/Examples/Character/Validation/AlphanumericCharacterValidation.java
@@ -0,0 +1,11 @@
+package Examples.Character.Validation;
+
+public class AlphanumericCharacterValidation {
+
+    /**
+     * 영숫자 집합 ([A-Za-z0-9]) 검사
+     */
+    public boolean validateAlphanumericCharacter(char c) {
+        return Character.isLetterOrDigit(c);
+    }
+}
