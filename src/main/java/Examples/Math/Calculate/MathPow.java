c428fd0bd907133c21a05bf08bdc6c2c827c3983
yechanpark <qkrdpcks0419@naver.com>
2020-02-04 23:34:16 +0900

    Math.pow()사용법 추가

---
+++ src/main/java/Examples/Math/Calculate/MathPow.java
@@ -0,0 +1,12 @@
+package Examples.Math.Calculate;
+
+/**
+ * 제곱 관련
+ */
+public class MathPow {
+    public int pow(int number) {
+        // number의 2승
+        // 주의 : 2^i는 논리연산자 XOR임
+        return (int) Math.pow(number, 2);
+    }
+}
