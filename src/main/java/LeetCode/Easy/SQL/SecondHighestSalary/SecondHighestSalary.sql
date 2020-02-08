022067114f611cd55a3a9ab17b0f2d6430bc5904
yechanparfk <qkrdpcks0419@naver.com>
2020-02-08 11:45:45 +0900

    LeetCode_Easy_SecondHighestSalary

---
+++ src/main/java/LeetCode/Easy/SQL/SecondHighestSalary/SecondHighestSalary.sql
@@ -0,0 +1,40 @@
+/*
+ - SQL Schema
+Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (Id, Salary) values ('1', '100')
+insert into Employee (Id, Salary) values ('2', '200')
+insert into Employee (Id, Salary) values ('3', '300')
+
+ - Combine Two Tables
+ Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+*/
+
+SELECT
+    MAX(Employee.Salary) AS SecondHighestSalary
+FROM
+    Employee
+WHERE
+    Employee.Salary NOT IN (
+        SELECT
+            MAX(Employee.Salary)
+        FROM
+            Employee
+    )
+
+
