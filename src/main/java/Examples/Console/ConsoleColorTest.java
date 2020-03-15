58120967230600b7d255d0b86551bd7e33f0a62f
yechanpark <qkrdpcks0419@naver.com>
2020-03-15 21:56:29 +0900

    Console 로그 컬러 예제 추가

---
+++ src/main/java/Examples/Console/ConsoleColorTest.java
@@ -0,0 +1,22 @@
+package Examples.Console;
+
+public class ConsoleColorTest {
+    public static void main(String[] args) {
+        System.out.println(ConsoleColor.ANSI_BLACK.toString("BLACK"));
+        System.out.println(ConsoleColor.ANSI_BLACK_BACKGROUND.toString("BLACK_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_BLUE.toString("BLUE"));
+        System.out.println(ConsoleColor.ANSI_BLUE_BACKGROUND.toString("BLUE_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_CYAN.toString("CYAN"));
+        System.out.println(ConsoleColor.ANSI_CYAN_BACKGROUND.toString("CYAN_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_GREEN.toString("GREEN"));
+        System.out.println(ConsoleColor.ANSI_GREEN_BACKGROUND.toString("GREEN_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_PURPLE.toString("PURPLE"));
+        System.out.println(ConsoleColor.ANSI_PURPLE_BACKGROUND.toString("PURPLE_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_RED.toString("RED"));
+        System.out.println(ConsoleColor.ANSI_RED_BACKGROUND.toString("RED_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_WHITE.toString("WHITE"));
+        System.out.println(ConsoleColor.ANSI_WHITE_BACKGROUND.toString("WHITE_BACKGROUND"));
+        System.out.println(ConsoleColor.ANSI_YELLOW.toString("YELLOW"));
+        System.out.println(ConsoleColor.ANSI_YELLOW_BACKGROUND.toString("YELLOW_BACKGROUND"));
+    }
+}
